3c4695aa1c44eda7ec80c9372a159fc1fd1276fa
Jimmy <jimmy@optily.com>
2022-05-21 10:19:29 +0100

    Adding cors config..

--- src/main/java/com/visible/thread/demo/config/WebConfig.java
+++ src/main/java/com/visible/thread/demo/config/WebConfig.java
@@ -1,10 +1,19 @@
 package com.visible.thread.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
 @EnableWebFlux
-public class WebConfig {
+public class WebConfig implements WebFluxConfigurer {
 
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:8081")
+                .allowedMethods("*")
+                .maxAge(3600);
+    }
 }
